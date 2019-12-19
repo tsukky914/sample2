b78b4524eaeec7357bdced17c33c7577262110e1
tsukky914 <test@test.com>
2019-12-19 22:40:42 +0900

    test

--- hogehoge222.rb
+++ hogehoge222.rb
@@ -1,5 +1,5 @@
 hgoehgoehgoehgoehge
-hgoehgoehgoehgoehge
+hgoehgo******************************ehgoehgoehge
 hgoehgoehgoehgoehgehgoehgoehgoehgoehgehgoehgoehgoehgoehge
 hgoehgoehgoehgoehgehgoehgoehgoehgoehge
 hgoehgoehgoehgoehge
